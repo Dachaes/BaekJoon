9b9ac8739363bb6b9e10cc1c20cbab293e4e1919
SOHYEON <121792448+Dachaes@users.noreply.github.com>
2023-03-13 18:06:55 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
@@ -0,0 +1,18 @@
+# BOJ_18406 : 럭키 스트레이트
+
+# Input
+score = input()
+len_score = len(score)
+
+left_sum = 0
+right_sum = 0
+for i in range(len_score):
+    if i < len_score / 2:
+        left_sum += int(score[i])
+    else:
+        right_sum += int(score[i])
+
+if left_sum == right_sum:
+    print("LUCKY")
+else:
+    print("READY")
