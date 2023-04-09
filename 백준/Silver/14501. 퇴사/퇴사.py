03b416998ca540341b57b74d4c210cfa3b54083e
SOHYEON <121792448+Dachaes@users.noreply.github.com>
2023-04-09 12:21:26 +0900

    [Silver III] Title: 퇴사, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,17 @@
+# BOJ_14501 : 퇴사
+
+# Input
+date = int(input())
+plan = [[0, 0]]
+for i in range(date):
+    plan.append(list(map(int, input().split())))
+
+max_profit = [0] * (date + 2)
+
+for i in range(date, 0, -1):
+    if i + plan[i][0] <= date + 1:
+        max_profit[i] = max(plan[i][1] + max_profit[i + plan[i][0]], max_profit[i + 1])
+    else:
+        max_profit[i] = max_profit[i + 1]
+
+print(max_profit[1])
