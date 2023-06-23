350dbf0de77ba8fc7350871acdead2f5996f0ab8
SOHYEON <121792448+Dachaes@users.noreply.github.com>
2023-06-23 15:17:52 +0900

    [level 4] Title: 가사 검색, Time: 9334.63 ms, Memory: 785 MB -BaekjoonHub

--- 프로그래머스/lv4/60060. 가사 검색/가사 검색.py
+++ 프로그래머스/lv4/60060. 가사 검색/가사 검색.py
@@ -1,31 +1,58 @@
-from bisect import bisect_left, bisect_right
+class Node:
+    def __init__(self, data):
+        self.letter = data
+        self.count = 0
+        self.children = {}
+        self.is_end_of_word = False
 
-# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
-def count_by_range(a, left_value, right_value):
-    right_index = bisect_right(a, right_value)
-    left_index = bisect_left(a, left_value)
-    return right_index - left_index
 
-# 모든 단어들을 길이마다 나누어서 저장하기 위한 리스트
-array = [[] for _ in range(10001)]
-# 모든 단어들을 길이마다 나누어서 뒤집어 저장하기 위한 리스트
-reversed_array = [[] for _ in range(10001)]
+class Trie:
+    def __init__(self):
+        self.root = Node(None)
+        self.count = 0
+    def insert(self, string):
+        current_node = self.root
+        self.count += 1
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+            current_node.count += 1
+        current_node.is_end_of_word = True
+
+    def search(self, string):
+        current_node = self.root
+        for char in string:
+            if char != '?':
+                if char in current_node.children:
+                    current_node = current_node.children[char]
+                else:
+                    return 0
+            else:
+                break
+
+        return current_node.count
+
 
 def solution(words, queries):
     answer = []
-    for word in words: # 모든 단어를 접미사 와일드카드 배열, 접두사 와일드카드 배열에 각각 삽입
-        array[len(word)].append(word) # 단어를 삽입
-        reversed_array[len(word)].append(word[::-1]) # 단어를 뒤집어서 삽입
+    trie_words, reversed_trie_words = {}, {}
+    for word in words:
+        if len(word) not in trie_words:
+            trie_words[len(word)] = Trie()
+            reversed_trie_words[len(word)] = Trie()
 
-    for i in range(10001): # 이진 탐색을 수행하기 위해 각 단어 리스트 정렬 수행
-        array[i].sort()
-        reversed_array[i].sort()
+        trie_words[len(word)].insert(word)
+        reversed_trie_words[len(word)].insert(word[::-1])
 
-    for q in queries: # 쿼리를 하나씩 확인하며 처리
-        if q[0] != '?': # 접미사에 와일드 카드가 붙은 경우
-            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
-        else: # 접두사에 와일드 카드가 붙은 경우
-            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
-        # 검색된 단어의 개수를 저장
-        answer.append(res)
+    for query in queries:
+        if len(query) not in trie_words:
+            answer.append(0)
+        elif len(query) == query.count('?'):
+            answer.append(trie_words[len(query)].count)
+        elif query[0] != '?':
+            answer.append(trie_words[len(query)].search(query))
+        else:
+            answer.append(reversed_trie_words[len(query)].search(query[::-1]))
     return answer
