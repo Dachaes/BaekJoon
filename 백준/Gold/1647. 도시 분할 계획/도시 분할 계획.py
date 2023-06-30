8b8f0ee457dc0467505183f78e3e78a3d98d676e
SOHYEON <121792448+Dachaes@users.noreply.github.com>
2023-06-30 12:55:11 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 5036 ms, Memory: 251244 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,42 @@
+# BOJ_1647 : 도시 분할 계획
+import sys
+
+# Input
+num_houses, num_paths = map(int, sys.stdin.readline().split())
+paths = []
+for _ in range(num_paths):
+    data = list(map(int, sys.stdin.readline().split()))
+    paths.append([data[2], data[0], data[1]])
+paths.sort()
+
+
+def find_parent(x):
+    global parent
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+
+def union(a, b):
+    global parent
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+# 크루스칼 알고리즘
+parent = [i for i in range(num_houses + 1)]
+last_cost = 0
+res = 0
+
+for path in paths:
+    cost, num1, num2 = path
+    if find_parent(num1) != find_parent(num2):
+        union(num1, num2)
+        res += cost
+        last_cost = cost
+
+print(res - last_cost)
