56177bbcb1e23e4b18fb20b8728a6355d6b5c702
SOHYEON <121792448+Dachaes@users.noreply.github.com>
2023-07-23 12:41:11 +0900

    [Silver III] Title: 파도반 수열, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,27 @@
+# BOJ_9461 : 파도반 수열
+import sys
+
+num_tests = int(sys.stdin.readline())
+n_list = []
+for _ in range(num_tests):
+    n_list.append(int(sys.stdin.readline()))
+
+
+def seq(num):
+    global sequence
+    if sequence[num] == -1:
+        if 1 <= num < 4:
+            sequence[num] = 1
+        elif 4 <= num < 6:
+            sequence[num] = 2
+        elif 6 <= num < 9:
+            sequence[num] = seq(num - 1) + seq(1)
+        else:
+            sequence[num] = seq(num - 1) + seq(num - 5)
+
+    return sequence[num]
+
+
+sequence = [-1] * 101
+for n in n_list:
+    print(seq(n))
