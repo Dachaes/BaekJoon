41456c4afbd96ca67359a0f1490cabede5552e46
SOHYEON <121792448+Dachaes@users.noreply.github.com>
2023-07-27 11:39:54 +0900

    [Bronze II] Title: 대회 자리, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5176. 대회 자리/대회 자리.py
@@ -0,0 +1,19 @@
+# BOJ_5176 : 대회 자리
+import sys
+INF = int(1e9)
+
+n_cases = int(sys.stdin.readline())
+
+answers = []
+for _ in range(n_cases):
+    n_participants, n_seats = map(int, sys.stdin.readline().split())
+    seats = []
+    for _ in range(n_participants):
+        seats.append(int(sys.stdin.readline()))
+    seats_set = set(seats)
+    len_seats_in_use = len(seats_set)
+
+    answers.append(n_participants - len_seats_in_use)
+
+for answer in answers:
+    print(answer)
