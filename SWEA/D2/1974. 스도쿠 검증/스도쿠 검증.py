22bbed49c707c41955d68f22002f3ac50b9ff841
SOHYEON <121792448+Dachaes@users.noreply.github.com>
2023-08-08 15:40:31 +0900

    [D2] Title: 스도쿠 검증, Time: 204 ms, Memory: 59,568 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -2,7 +2,18 @@ from copy import deepcopy
 
 n_tests = int(input())
 
+
+def my_count(datas, key):
+	count = 0
+	for data in datas:
+		if data == key:
+			count += 1
+	return count
+
+
 for t in range(n_tests):
+	# numbers: 스토쿠 행렬
+	# numbers_T: 스토쿠 행렬의 전치 행렬
 	numbers = [list(map(int, input().split())) for _ in range(9)]
 	numbers_T = deepcopy(numbers)
 	for i in range(9):
@@ -11,29 +22,34 @@ for t in range(n_tests):
 				numbers_T[i][j], numbers_T[j][i] = numbers_T[j][i], numbers_T[i][j]
 
 	result = 1
-	# 가로
+	# 가로 행을 매번 체크할 때마다 체크 리스트를 만들어서
+	# 1부터 9까지 각각 1개씩 있는지 확인한다.
 	for number in numbers:
 		cnt_list = [0] * 9
 		for num in number:
 			cnt_list[num - 1] += 1
-		if cnt_list.count(1) != 9:
+		if my_count(cnt_list, 1) != 9:
 			result = 0
-	# 세로
+
+	# 세로 열을 매번 체크할 때마다 체크 리스트를 만들어서
+	# 1부터 9까지 각각 1개씩 있는지 확인한다.
 	if result:
 		for number in numbers_T:
 			cnt_list = [0] * 9
 			for num in number:
 				cnt_list[num - 1] += 1
-			if cnt_list.count(1) != 9:
+			if my_count(cnt_list, 1) != 9:
 				result = 0
-	# 칸 별로
+
+	# 칸 별로 매번 체크할 때마다 체크 리스트를 만들어서
+	# 1부터 9까지 각각 1개씩 있는지 확인한다.
 	for i in range(0, 7, 3):
 		for j in range(0, 7, 3):
 			cnt_list = [0] * 9
 			for k in range(3):
 				for l in range(3):
 					cnt_list[numbers[i+k][j+l] - 1] += 1
-			if cnt_list.count(1) != 9:
+			if my_count(cnt_list, 1) != 9:
 				result = 0
 
 	print(f"#{t + 1}", result)
