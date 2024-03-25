1603318e5c6e769e9671904abf7bf4eda41772c1
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-03-25 15:41:06 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -1,41 +1,24 @@
-// BOJ_2444 : 별 찍기 7
 #include <iostream>
 using namespace std;
 
-int i, j, N, N_MAX;
-void Print_star7();
-
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0);
-    cin >> N;
-    N_MAX = 2*N - 1;
-    Print_star7();
-    return 0;
-}
+    int n;
+    cin >> n;
 
-void Print_star7(){
-    for(i = 0; i < N_MAX; i++){
-        if(i >= 0 && i < N-1){
-            for(j = 0; j < (N_MAX+1)/2 + i; j++){
-                if(j < N-(i+1))
-                    cout << " ";
-                else
-                    cout << "*";
-            }
-        }
-        else if(i == N-1){
-             for(j = 0; j < N_MAX; j++)
-                    cout << "*";
-        }
-
-        else{
-            for(j = 0; j < N_MAX - (i - N + 1); j++){
-                if (j >= i - N + 1)
-                    cout << "*";
-                else
-                    cout << " ";
-            }
-        }
-        cout << endl;
+    int max = 2 * (n - 1) + 1;
+    char star = '*';
+    string padding;
+    string stars;
+    for (int i = 0; i < n; i++) {
+        stars = string(2 * i + 1, star);
+        padding = string((max - stars.size()) / 2, ' ');
+        cout << padding << stars << '\n';
     }
+    for (int i = n - 2; i >= 0; i--) {
+        stars = string(2 * i + 1, star);
+        padding = string((max - stars.size()) / 2, ' ');
+        cout << padding << stars << '\n';
+    }
+
+    return 0;
 }
