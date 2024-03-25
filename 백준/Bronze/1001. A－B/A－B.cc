9eaadae8df0453049f8f640b147cfd8c5052d412
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-03-25 13:01:08 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.cc
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int a, b;
-
     cin >> a >> b;
-    int c = a - b;
-    cout << c << endl;
+    cout << a - b << endl;
+
     return 0;
 }
