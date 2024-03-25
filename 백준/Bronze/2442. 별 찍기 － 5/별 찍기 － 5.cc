1bc3ace68b568bfdf72e6a421c99393234bfd59b
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-03-25 15:31:06 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -1,26 +1,19 @@
-// BOJ_2442 : 별 찍기 5
 #include <iostream>
 using namespace std;
 
-int i, j, N, N_MAX;
-void Print_star5();
-
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0);
-    cin >> N;
-    N_MAX = 2*N - 1;
-    Print_star5();
-    return 0;
-}
+    int n;
+    cin >> n;
 
-void Print_star5(){
-    for(i = 0; i < N; i++){
-        for(j = 0; j < (N_MAX+1)/2 + i; j++){
-            if (j >= N-i-1 && j <= N+i-1)
-                cout << "*";
-            else
-                cout << " ";
-        }
-        cout << endl;
+    int max = 2 * (n - 1) + 1;
+    char star = '*';
+    string padding;
+    string stars;
+    for (int i = 0; i < n; i++) {
+        stars = string(2 * i + 1, star);
+        padding = string((max - stars.size()) / 2, ' ');
+        cout << padding << stars << '\n';
     }
+
+    return 0;
 }
