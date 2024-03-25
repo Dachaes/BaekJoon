7d7545976b6b7fe79f2b91d7d5c35ae8d9e27958
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-03-25 12:54:35 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,30 +1,10 @@
-#include <bits/stdc++.h>
-
-#define FOR(i, n, m) for (int i=(n); i<=(m); i++)
-#define si(n) fscanf(IN, "%d", &n)
-
+#include <iostream>
 using namespace std;
 
-typedef long long ll;
-FILE *IN = stdin, *OUT = stdout;
-
-int a, b, c;
-
-void in() {
-    ios::sync_with_stdio(false); cin.tie(0);
-    si(a), si(b);
-}
-
-void solve() {
-    c = a + b;
-}
-
-void out() {
-    printf("%d\n", c);
-}
-
 int main() {
-    in();
-    solve();
-    out();
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
 }
