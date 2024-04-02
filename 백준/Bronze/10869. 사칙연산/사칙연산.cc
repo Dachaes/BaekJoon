4b34139584ca032f9a154efc7ab601c2703ff6da
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-04-03 08:44:53 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 0, b = 0;
+    cin >> a >> b;
+
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+
+    return 0;
+}
