6423d72fc407c575c3d30e72f646281a5d3fc801
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-07-23 14:58:18 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    // 입력 - 카드 덱
+    int n_commands = 0;
+    cin >> n_commands;
+    cin.ignore();
+
+    queue<int> numbers;
+    for (int i = 0; i < n_commands; ++i) {
+        string command;
+        getline(cin, command);
+
+        // empty
+        if (command == "empty") {
+            if (numbers.empty())
+                cout << 1 << '\n';
+            else
+                cout << 0 << '\n';
+        }
+        // size
+        else if (command == "size") {
+            cout << numbers.size() << '\n';
+        }
+        // front
+        else if (command == "front") {
+            if (numbers.empty())
+                cout << -1 << '\n';
+            else
+                cout << numbers.front() << '\n';
+        }
+        // back
+        else if (command == "back") {
+            if (numbers.empty())
+                cout << -1 << '\n';
+            else
+                cout << numbers.back() << '\n';
+        }
+        // pop
+        else if (command == "pop") {
+            if (numbers.empty())
+                cout << -1 << '\n';
+            else {
+                cout << numbers.front() << '\n';
+                numbers.pop();
+            }
+        }
+        // push
+        else {
+            int number = stoi(command.substr(5));
+            numbers.emplace(number);
+        }
+    }
+
+    return 0;
+}
