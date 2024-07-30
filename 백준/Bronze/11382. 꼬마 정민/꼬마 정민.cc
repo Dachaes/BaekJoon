cf3345c827ee9033bc7c23ce527cc7f2aa580828
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-07-30 16:49:08 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    // 입력
+    long long a = 0, b = 0, c = 0;
+    cin >> a >> b >> c;
+    
+    cout << a + b + c << '\n';
+    return 0;
+}
