4ed58f60340bcca6d174dbc7be268bfc31768e04
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-08-10 20:47:18 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    // 입력
+    int n_numbers = 0;         cin >> n_numbers;
+    priority_queue<int, vector<int>, greater<int>> numbers;
+
+    int number = 0;
+    for (int i = 0; i < n_numbers; ++i) {
+        cin >> number;
+
+        if (number == 0) {
+            if (numbers.empty()) {
+                cout << '0' << '\n';
+            }
+            else {
+                number = numbers.top();
+                numbers.pop();
+                cout << number << '\n';
+            }
+        }
+        else {
+            numbers.emplace(number);
+        }
+    }
+
+    return 0;
+}
