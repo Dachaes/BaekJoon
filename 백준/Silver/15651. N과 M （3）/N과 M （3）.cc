17c5856b02d495f31426571c0e2e5b68f51f2995
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-08-15 21:53:40 +0900

    [Silver III] Title: N과 M (3), Time: 320 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void NM3(int, int, vector<int>&);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // 입력
+    int n_numbers = 0;    cin >> n_numbers;
+    int n_seq = 0;       cin >> n_seq;
+
+    vector<int> nm3;
+    NM3(n_numbers, n_seq, nm3);
+    return 0;
+}
+
+
+void NM3(int n_numbers, int n_seq, vector<int>& nm3) {
+    if (nm3.size() == n_seq) {
+        for (int nm : nm3)
+            cout << nm << ' ';
+        cout << '\n';
+
+        return;
+    }
+
+    for (int i = 1; i <= n_numbers; ++i) {
+        nm3.push_back(i);
+        NM3(n_numbers, n_seq, nm3);
+        nm3.pop_back();
+    }
+}
