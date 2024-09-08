c8222528d003a03a97833c7ba0d0a35fe2b04ed6
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2024-09-09 01:05:22 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -9,20 +9,19 @@ int main() {
     cout.tie(nullptr);
 
     string lasers;       cin >> lasers;
-    stack<char> s_laser;
 
-    int answer = 0;
+    int n_sticks = 0, answer = 0;
     for (int i = 0; i < lasers.length(); ++i) {
         char laser = lasers[i];
         if (laser == '(') {
-            s_laser.emplace(laser);
+            n_sticks++;
         }
         else if (laser == ')') {
-            s_laser.pop();
+            n_sticks--;
             char prev_laser = lasers[i - 1];
             // 레이저 o
             if (prev_laser == '(')
-                answer += s_laser.size();
+                answer += n_sticks;
             // 레이저 x
             else if (prev_laser == ')')
                 answer += 1;
