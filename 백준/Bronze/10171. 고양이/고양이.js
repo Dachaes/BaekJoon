99479cd6f02c63a9d14ec5ef8c7d9072222e39ba
Sohyeon <121792448+Dachaes@users.noreply.github.com>
2025-09-01 13:00:47 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split(' ');
+
+console.log("\\    /\\");
+console.log(" )  ( ')");
+console.log("(  /  )");
+console.log(" \\(__)|");
